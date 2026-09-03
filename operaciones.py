c677d93a62060f153866a5c01d56c0158cbb3cf6
Andres Parra <andresparrareyes10@gmail.com>
2026-09-03 10:11:49 -0500

    mi primer commit

---
+++ operaciones.py
@@ -0,0 +1,11 @@
+numero1,numero2 =map(int,input("ingrese dos numeros:").split())
+
+suma = numero1 + numero2
+resta = numero1 + numero2
+multiplicacion = numero1 + numero2
+division = numero1 + numero2
+
+print("LA SUMA DE LOS NUMEROS ES:",suma)
+print("LA RESTA DE LOS NUMEROS ES", resta)
+print("LA MULTIPLICACION DE LOS NUMEROS ES", multiplicacion)
+print("LA RDIVISION DE LOS NUMEROSCES", division)
